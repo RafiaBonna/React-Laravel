73957811bca275ae1789ef5897dc54c1895ce255
RafiaBonna <rafiaakterbonna@gmail.com>
2025-11-29 23:33:50 +0600

    my

--- Re-Lar/react_frontend/vite-project/src/components/Edit.jsx
+++ Re-Lar/react_frontend/vite-project/src/components/Edit.jsx
@@ -33,7 +33,7 @@ function ProductForm({ loadProducts }) {
         required
         style={{ marginRight: 10 }}
       />
-      <button type="submit">Add Product</button>
+      <button type="submit">Up Product</button>
     </form>
   );
 }
